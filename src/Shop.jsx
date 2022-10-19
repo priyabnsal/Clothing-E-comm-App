a2579a4e3b8d78b2ef98c77b20b6dc04e5b36455
Priya Bansal <priyabansal.cse@gmail.com>
2022-10-19 23:48:28 +0530

    Created till cart but w/o functioanlity

---
+++ src/Shop.jsx
@@ -0,0 +1,15 @@
+const Shop = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>Shop</li>
+                    <li>Seller</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+      </>
+  )
+}
+export default Shop
